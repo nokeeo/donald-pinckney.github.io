7111ff1ab4399d4b39247a4eaa6a72ee93151357
Donald Pinckney <donald_pinckney@icloud.com>
2017-11-16 09:25:48 -0800

    Travis?

--- books/build_book.sh
+++ books/build_book.sh
@@ -30,13 +30,21 @@ bookName=$(basename $bookDir)
 
 currDir=$(pwd)
 echo "In directory: $currDir"
+
 echo "Copying src/ to $srcDir/../_drafts/$bookName"
-echo "Copying src/ to $srcDir/../_posts/$bookName"
 tree $srcDir/..
-cp -R src/ $srcDir/../_drafts/$bookName/
+rm -rf $srcDir/../_drafts/$bookName/
+mkdir $srcDir/../_drafts/$bookName
+cp -R src/* $srcDir/../_drafts/$bookName/
 tree $srcDir/..
 
-cp -R src/ $srcDir/../_posts/$bookName/
+echo "Copying src/ to $srcDir/../_posts/$bookName"
+
+rm -rf $srcDir/../_drafts/$bookName/
+mkdir $srcDir/../_posts/$bookName
+cp -R src/* $srcDir/../_posts/$bookName/
+tree $srcDir/..
+
 rm $srcDir/../_drafts/$bookName/SUMMARY.md
 rm $srcDir/../_posts/$bookName/SUMMARY.md
 
