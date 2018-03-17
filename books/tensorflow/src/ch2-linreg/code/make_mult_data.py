90194502ccaf59058cf2d023a011cef4886f3d5e
Travis CI <donald_pinckney@icloud.com>
2018-03-17 04:42:41 +0000

    Deploy to GitHub Pages: 15278b072d3a094457be1cbd5b72b86bd9a88672

--- books/tensorflow/src/ch2-linreg/code/make_mult_data.py
+++ books/tensorflow/src/ch2-linreg/code/make_mult_data.py
@@ -4,23 +4,23 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 
-n = 20
+n = 400
 xr = np.linspace(0, 10, n)
 yr = np.linspace(0, 10, n)
 xs, ys = np.meshgrid(xr, yr)
-xs = np.reshape(xs, 400)
-ys = np.reshape(ys, 400)
+xs = np.reshape(xs, n*n)
+ys = np.reshape(ys, n*n)
 
 fs = 2*xs + 1.3*ys + 4
-rs = np.random.randn(400)
+rs = np.random.randn(n*n)
 rfs = fs + 3*rs
 
 D = np.column_stack((xs, ys, rfs))
 print(D.shape)
-np.savetxt('temp_data.csv', D, delimiter=',')
+np.savetxt('linreg-multi-synthetic-2.csv', D, delimiter=',')
 # print(D)
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.scatter(xs, ys, rfs)
-plt.show()
+# fig = plt.figure()
+# ax = fig.add_subplot(111, projection='3d')
+# ax.scatter(xs, ys, rfs)
+# plt.show()
--- books/tensorflow/src/ch2-linreg/code/multi_var_reg.py
+++ books/tensorflow/src/ch2-linreg/code/multi_var_reg.py
@@ -7,10 +7,12 @@ import matplotlib.pyplot as plt
 D = np.matrix(pd.read_csv("linreg-multi-synthetic-2.csv").values)
 
 # We extract all rows and the first 2 columns into X_data
-X_data = D[:, 0:2]
+# Then we flip it
+X_data = D[:, 0:2].transpose()
 
 # We extract all rows and the last column into y_data
-y_data = D[:, 2]
+# Then we flip it
+y_data = D[:, 2].transpose()
 
 # And make a convenient variable to remember the number of input columns
 n = 2
@@ -24,22 +26,22 @@ A = tf.get_variable("A", shape=(1, n))
 b = tf.get_variable("b", shape=())
 
 # Define model output
-y_predicted = A*x + b
+y_predicted = tf.matmul(A, x) + b
 
 # Define the loss function
 L = tf.reduce_sum((y_predicted - y)**2)
 
 # Define optimizer object
-optimizer = tf.train.AdamOptimizer(learning_rate=0.2).minimize(L)
+optimizer = tf.train.AdamOptimizer(learning_rate=0.1).minimize(L)
 
 # Create a session and initialize variables
 session = tf.Session()
 session.run(tf.global_variables_initializer())
 
 # Main optimization loop
-for t in range(10000):
+for t in range(2000):
     _, current_loss, current_A, current_b = session.run([optimizer, L, A, b], feed_dict={
         x: X_data,
         y: y_data
     })
-    print("t = %g, loss = %g, a = %s, b = %g" % (t, current_loss, str(current_A), current_b))
+    print("t = %g, loss = %g, A = %s, b = %g" % (t, current_loss, str(current_A), current_b))
