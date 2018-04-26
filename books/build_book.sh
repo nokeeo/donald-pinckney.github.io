46c748ba28544430c608b7f15758b3f1cc986d7b
Travis CI <donald_pinckney@icloud.com>
2018-04-26 01:10:18 +0000

    Deploy to GitHub Pages: 9e2c0b716bcdf0ae8c3167088802487999e1fe18

--- books/build_book.sh
+++ books/build_book.sh
@@ -2,8 +2,8 @@
 
 srcDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-bookDir=$1
-cd $bookDir
+bookDir="$1"
+cd "$bookDir"
 
 # Get list of all markdown files in book
 files=$(find src -name "*.md")
@@ -30,24 +30,24 @@ done
 
 
 # Copy the book to _drafts and _posts
-bookName=$(basename $bookDir)
+bookName=$(basename "$bookDir")
 
 currDir=$(pwd)
 echo "In directory: $currDir"
 
 echo "Copying src/ to $srcDir/../_drafts/$bookName"
-rm -rf $srcDir/../_drafts/$bookName/
-mkdir $srcDir/../_drafts/$bookName
-cp -R src/* $srcDir/../_drafts/$bookName/
+rm -rf "$srcDir/../_drafts/$bookName/"
+mkdir "$srcDir/../_drafts/$bookName"
+cp -R src/* "$srcDir/../_drafts/$bookName/"
 
 echo "Copying src/ to $srcDir/../_posts/$bookName"
 
-rm -rf $srcDir/../_posts/$bookName/
-mkdir $srcDir/../_posts/$bookName
-cp -R src/* $srcDir/../_posts/$bookName/
+rm -rf "$srcDir/../_posts/$bookName/"
+mkdir "$srcDir/../_posts/$bookName"
+cp -R src/* "$srcDir/../_posts/$bookName/"
 
-rm $srcDir/../_drafts/$bookName/SUMMARY.md
-rm $srcDir/../_posts/$bookName/SUMMARY.md
+rm "$srcDir/../_drafts/$bookName/SUMMARY.md"
+rm "$srcDir/../_posts/$bookName/SUMMARY.md"
 
 
 
