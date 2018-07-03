49f688504335780a953c5cf4209759be3e7627a6
Donald Pinckney <donald_pinckney@icloud.com>
2018-07-03 10:56:39 -0700

    Work on setting up shaders

---
+++ public/post_assets/metal/metal-intro-1/MetalIntro1/MetalIntro1/ShaderDefinitions.h
@@ -0,0 +1,13 @@
+//
+//  ShaderDefinitions.h
+//  MetalIntro1
+//
+//  Created by Donald Pinckney on 7/2/18.
+//  Copyright © 2018 donaldpinckney. All rights reserved.
+//
+
+#ifndef ShaderDefinitions_h
+#define ShaderDefinitions_h
+
+
+#endif /* ShaderDefinitions_h */
