028fcbd63edfcb7540b0be794774c308d816ff9f
Donald Pinckney <donald_pinckney@icloud.com>
2018-10-28 00:06:22 -0400

    Work on plotting results

--- books/tensorflow/src/ch2-linreg/code/scaling_temp.py
+++ books/tensorflow/src/ch2-linreg/code/scaling_temp.py
@@ -5,53 +5,55 @@ import matplotlib.pyplot as plt
 
 ### Hyperparameters ###
 
-LEARNING_RATE = 0.00005
+# fs = 2*xs + 0.013*ys + 4
+
+LEARNING_RATE = 0.01 # 0.0000025
 OPTIMIZER_CONSTRUCTOR = tf.train.GradientDescentOptimizer
-NUM_ITERS = 20000
-DIV_X = 1
-DIV_Y = 30
-SUB_Y = 13
+NUM_ITERS = 5000
+DIV_Y = 100 # 1
+# DIV_X_1 = 1
+# SUB_Y = 13
 
-# Load the data, and convert to 1x30 vectors
-D = pd.read_csv("homicide.csv")
-x_data = np.matrix(D.age.values) / DIV_X
-y_data = np.matrix(D.num_homicide_deaths.values) / DIV_Y
+# First we load the entire CSV file into an m x 3
+D = np.matrix(pd.read_csv("linreg-scaling-synthetic.csv", header=None).values)
 
-
-### Model definition ###
-
-# Define x (input data) placeholder
-x = tf.placeholder(tf.float32, shape=(1, None))
-
-# Define the trainable variables
-a = tf.get_variable("a", shape=())
-b = tf.get_variable("b", shape=())
-
-# Define the prediction model
-y_predicted = a*x + b
+# We extract all rows and the first 2 columns into X_data
+# Then we flip it
+X_data = D[:, 0:2].transpose()
+X_data[1, :] = X_data[1, :] / DIV_Y
 
 
-### Loss function definition ###
+# We extract all rows and the last column into y_data
+# Then we flip it
+y_data = D[:, 2].transpose()
 
-# Define y (correct data) placeholder
+# And make a convenient variable to remember the number of input columns
+n = 2
+
+# Define data placeholders
+x = tf.placeholder(tf.float32, shape=(n, None))
 y = tf.placeholder(tf.float32, shape=(1, None))
 
+# Define trainable variables
+A = tf.get_variable("A", initializer=tf.constant([1.5, 0.1 * DIV_Y], shape=(1, n)))
+# b = tf.get_variable("b", initializer=tf.constant(1, dtype=tf.float32))
+
+# Define model output
+y_predicted = tf.matmul(A, x)
+
 # Define the loss function
-L = tf.reduce_sum((y_predicted - y)**2)
+L = tf.reduce_mean((y_predicted - y)**2)
 
 
-### Summary setup ###
+# log_name = "%g; %g, %s" % (DIV_Y, LEARNING_RATE, OPTIMIZER_CONSTRUCTOR.__name__)
+# tf.summary.scalar('A_0', A[0,0])
+# tf.summary.scalar('A_1', A[0,1] / DIV_Y)
+# # tf.summary.scalar('b', b)
+# tf.summary.scalar('L', L)
+# summary_node = tf.summary.merge_all()
+# summary_writer = tf.summary.FileWriter('/tmp/tensorflow/scaling_temp/' + log_name)
+# print("Open /tmp/tensorflow/scaling_temp/ with tensorboard")
 
-log_name = "%g,%g,%g; %g, %s" % (DIV_X, DIV_Y, SUB_Y, LEARNING_RATE, OPTIMIZER_CONSTRUCTOR.__name__)
-tf.summary.scalar('a', a)
-tf.summary.scalar('b', b)
-tf.summary.scalar('L', L)
-summary_node = tf.summary.merge_all()
-summary_writer = tf.summary.FileWriter('/tmp/tensorflow/scaling_temp/' + log_name)
-print("Open /tmp/tensorflow/scaling_temp/ with tensorboard")
-
-
-### Training the model ###
 
 # Define optimizer object
 optimizer = OPTIMIZER_CONSTRUCTOR(learning_rate=LEARNING_RATE).minimize(L)
@@ -60,12 +62,44 @@ optimizer = OPTIMIZER_CONSTRUCTOR(learning_rate=LEARNING_RATE).minimize(L)
 session = tf.Session()
 session.run(tf.global_variables_initializer())
 
+run_data = np.empty((0, 3))
+
 # Main optimization loop
 for t in range(NUM_ITERS):
-    _, summary = session.run([optimizer, summary_node], feed_dict={
-        x: x_data,
+    _, current_loss, current_A = session.run([optimizer, L, A], feed_dict={
+        x: X_data,
         y: y_data
     })
-    summary_writer.add_summary(summary, t)
-    # print("t = %g, loss = %g, a = %g, b = %g" % (t, current_loss, current_a, current_b))
+    row = np.array([current_A[0,0], current_A[0,1], current_loss])
+    run_data = np.append(run_data, [row], axis=0)
+    # summary_writer.add_summary(summary, t)
+    # print("t = %g, loss = %g" % (t, current_loss))
 
+
+start_idx = 20
+end_idx = run_data.shape[0]
+
+print(run_data)
+
+old_data = np.load('run1.npy')
+
+plt.figure(num=None, figsize=(10, 4), dpi=80)
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif')
+
+plt.subplot(1, 2, 1)
+plt.plot(np.arange(start_idx, end_idx), old_data[start_idx:,0])
+plt.plot(np.arange(start_idx, end_idx), run_data[start_idx:,0])
+plt.title('Value of $A_0$')
+plt.xlabel('Iteration')
+plt.ylabel('$A_0$')
+
+
+plt.subplot(1, 2, 2)
+plt.plot(np.arange(start_idx, end_idx), old_data[start_idx:,2])
+plt.plot(np.arange(start_idx, end_idx), run_data[start_idx:,2])
+plt.title('Value of the loss function')
+plt.xlabel('Iteration')
+plt.ylabel('Loss function')
+
+plt.show()
